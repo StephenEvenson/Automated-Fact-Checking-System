629f728df1d1c0dac5697ad0dcfba3b277b51bf2
StephenEveson <1258455347@qq.com>
2023-05-13 09:38:38 +1000

    rerank

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from preprocess import get_train_data
-from train import retrieve_train
+from train import retrieve_train, rerank_train
 
 
 def main():
-    retrieve_train(epochs=1000)
+    retrieve_train(epochs=100)
+    rerank_train(epochs=60)
 
 
 if __name__ == '__main__':
