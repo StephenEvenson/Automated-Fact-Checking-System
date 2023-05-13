3e9539510f44d03a13f77ff09d77a4f8c8fb99c4
StephenEveson <1258455347@qq.com>
2023-05-14 01:40:16 +1000

    classifier

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from train import retrieve_train, rerank_train, classifier_train
 
 
 def main():
-    retrieve_train(epochs=20)
+    retrieve_train(epochs=100)
     rerank_train(epochs=20, load_old_model=False)
     classifier_train(epochs=20, load_old_model=False)
     # get_test_claim_result()
