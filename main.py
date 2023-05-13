236950102620c58f9d13120ff38c052d17e497a6
StephenEveson <1258455347@qq.com>
2023-05-14 07:38:17 +1000

    predict

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from train import retrieve_train, rerank_train, classifier_train
 
 def main():
     # retrieve_train(epochs=100)
-    rerank_train(epochs=20, load_old_model=False)
-    classifier_train(epochs=20, load_old_model=False)
-    # get_test_claim_result()
+    # rerank_train(epochs=20, load_old_model=False)
+    # classifier_train(epochs=20, load_old_model=False)
+    get_test_claim_result()
 
 if __name__ == '__main__':
     main()
